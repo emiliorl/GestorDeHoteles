374b8f114cd2f191ea0bc2089d01fd072417ca97
Matthew Reyes <mreyes-20161552@kinal.edu.gt>
2021-05-02 20:04:25 -0600

    Conflicto de fusión resuleto

--- app.js
+++ app.js
@@ -2,14 +2,7 @@
 
 var express = require('express');
 var bodyParser =  require('body-parser');
-<<<<<<< HEAD
-var userRoutes = require('./routes/user.route');
-var companyRoutes = require('./routes/company.route')
-var employeeRoutes = require('./routes/employee.route');
-=======
 var userRoute = require('./routes/user.route');
->>>>>>> ae7666c (se completa la estructura, se crea el jwt y authorizated, modificacion en app, se comienza a trabajar con usuario)
-
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,12 +16,5 @@ app.use((req, res, next) => {
 	next();
 });
 
-<<<<<<< HEAD
-app.use('/api', userRoutes);
-app.use('/api', companyRoutes);
-app.use('/api', employeeRoutes)
-=======
 app.use('/v1', userRoute);
->>>>>>> ae7666c (se completa la estructura, se crea el jwt y authorizated, modificacion en app, se comienza a trabajar con usuario)
-
 module.exports = app;
